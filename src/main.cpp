6decfafe6bbe3d06961e5cccc5b44472dba70eb2
alvinli04 <alvin.li.j@gmail.com>
2022-02-12 11:28:12 -0500

    finished recursive tree

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,29 @@
 #include "draw.hpp"
 #include "color_constants.hpp"
 
-const int N = 1000;
+#define N 1000
+#define PI 3.1415926535
 
-void rec (int itr, int x, int y, double dir, int len, picture& p, const color& c) {
+const color color_list[8] = {RED, LIME, BLUE, YELLOW, CYAN, MAGENTA, GREEN, TEAL};
+
+void tree (int itr, int x, int y, double dir, int len, picture& p) {
+	if (!itr) return;
+	int newx = x + len * std::cos(dir), newy = y + len * std::sin(dir);
+	color c;
+	if (itr > 9) c = WHITE;
+	else c = color_list[itr % 8];
+
+	draw_line (x, y, newx, newy, p, c);
 	
+	tree (itr - 1, newx, newy, dir + PI / 6, len * 3 / 4, p);
+	tree (itr - 1, newx, newy, dir - PI / 6, len * 3 / 4, p);
 }
 
 int main () {
 	
     picture s(N, N);
 	
-	// usage: draw_line (x0, y0, x1, y1, s, c)
-	
-	// idea: fractal canopy thing
+	tree (15, 500, 150, PI/2, 200, s);
 
     s.to_ppm("test"); 
 	
