f8ff2c8239291f09ab17bb3bd2b1cabd1b1b9f7f
alvinli04 <alvin.li.j@gmail.com>
2022-03-21 10:48:04 -0400

    integer based for loops for torus and sphere

--- src/parametric.cpp
+++ src/parametric.cpp
@@ -5,10 +5,12 @@
 #include "matrix.hpp"
 #include "parametric.hpp"
 
+const int density = 20;
+
 // draws a circle centered at (cx, cy, cz)
 void circle (double cx, double cy, double cz, double r, edgelist& E){
 	auto x = [=] (double t) {
-		return cx + r * std::cos (2 * t * PI);	
+		return cx + r * std::cos (2 * t * PI);
 	};
 	auto y = [=] (double t) {
 		return cy + r * std::sin (2 * t * PI);
@@ -20,7 +22,7 @@ void circle (double cx, double cy, double cz, double r, edgelist& E){
 		if (rx != -1) E.add_edge (rx, ry, cz, currx, curry, cz);
 		rx = currx;
 		ry = curry;
-	}	
+	}
 }
 
 // draws a hermite curve
@@ -50,7 +52,7 @@ void hermite (double x0, double y0, double x1, double y1, double rx0, double ry0
 		double b = x_coeffs[1][0];
 		double c = x_coeffs[2][0];
 		double d = x_coeffs[3][0];
-		return a * t * t * t + b * t * t + c * t + d;	
+		return a * t * t * t + b * t * t + c * t + d;
 	};
 	auto y = [&] (double t) {
 		double a = y_coeffs[0][0];
@@ -96,7 +98,7 @@ void bezier (double x0, double y0, double x1, double y1, double x2, double y2, d
 		double b = x_coeffs[1][0];
 		double c = x_coeffs[2][0];
 		double d = x_coeffs[3][0];
-		return a * t * t * t + b * t * t + c * t + d;	
+		return a * t * t * t + b * t * t + c * t + d;
 	};
 	auto y = [&] (double t) {
 		double a = y_coeffs[0][0];
@@ -121,7 +123,7 @@ void box (double x, double y, double z, double width, double height, double dept
 	E.add_edge (x, y, z + depth, x + width, y, z + depth);
 	E.add_edge (x + width, y, z + depth, x + width, y, z);
 	E.add_edge (x + width, y, z, x, y, z);
-	
+
 	E.add_edge (x, y, z, x, y + height, z);
 	E.add_edge (x, y, z + depth, x, y + height, z + depth);
 	E.add_edge (x + width, y, z + depth, x + width, y + height, z + depth);
@@ -135,15 +137,18 @@ void box (double x, double y, double z, double width, double height, double dept
 
 // draws a sphere
 void sphere (double x, double y, double z, double r, edgelist& E) {
+	// generate points
 	std::vector<std::vector<double>> points;
-	for (double phi = 0; phi <= 1; phi += 0.02) {
-		for (double theta = 0; theta <= 0.5; theta += 0.02) {
-			double sx = r * std::cos (2 * PI * theta) + x;
-			double sy = r * std::sin (2 * PI * theta) * std::cos (2 * PI * phi) + y;
-			double sz = r * std::sin (2 * PI * theta) * std::sin (2 * PI * phi) + z;
+	for (int phi = 0; phi < density; ++phi) {
+		for (int theta = 0; theta <= density; ++theta) {
+			double sx = r * std::cos (PI * theta / density) + x;
+			double sy = r * std::sin (PI * theta / density) * std::cos (2 * PI * phi / density) + y;
+			double sz = r * std::sin (PI * theta / density) * std::sin (2 * PI * phi / density) + z;
 			points.push_back ( {sx, sy, sz} );
 		}
 	}
+
+	// add points to edge matrix
 	for (std::vector<double>& p : points) {
 		E.add_edge (p[0], p[1], p[2], p[0], p[1], p[2]);
 	}
@@ -151,20 +156,19 @@ void sphere (double x, double y, double z, double r, edgelist& E) {
 
 // draws a torus
 void torus (double x, double y, double z, double r1, double r2, edgelist& E) {
+	// generate points
 	std::vector<std::vector<double>> points;
-	for (double phi = 0; phi <= 1; phi += 0.02) {
-		for (double theta = 0; theta <= 1; theta += 0.02) {
-			double sx = (r1 * std::cos (2 * PI * theta) + r2) * std::cos (2 * PI * phi) + x;
-			double sy = r1 * std::sin (2 * PI * theta) + y;
-			double sz = -(r1 * std::cos (2 * PI * theta) + r2) * std::sin (2 * PI * phi) + z;
+	for (int phi = 0; phi < density; ++phi) {
+		for (int theta = 0; theta < density; ++theta) {
+			double sx = (r1 * std::cos (2 * PI * theta / density) + r2) * std::cos (2 * PI * phi / density) + x;
+			double sy = r1 * std::sin (2 * PI * theta / density) + y;
+			double sz = -(r1 * std::cos (2 * PI * theta / density) + r2) * std::sin (2 * PI * phi / density) + z;
 			points.push_back ( {sx, sy, sz} );
 		}
 	}
+
+	// add points to edge matrix
 	for (std::vector<double>& p : points) {
 		E.add_edge (p[0], p[1], p[2], p[0], p[1], p[2]);
 	}
 }
-
-
-
-
