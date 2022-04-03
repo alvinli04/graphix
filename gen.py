303ef4293eab21b36512cc959f3d59888d2db5a2
alvinli04 <alvin.li.j@gmail.com>
2022-04-03 12:11:16 -0400

    finished star collison

--- gen.py
+++ gen.py
@@ -1,2 +1,16 @@
-for i in range(90):
-    print(f'ident\nmove\n-320 -100 -400\nrotate\ny 4\nmove\n320 100 400\napply\nsave\nframe{str(i).zfill(2) }.png')
+import sys
+import math
+
+f = open('test.dw', 'w')
+
+for t in range(100):
+    theta = t * 10 + 0.25 * t * t
+    r = 100 - t
+    f.write(f'clear\nsphere\n{-1*r} 0 0 30\nsphere\n{r} 0 0 30\nident\nrotate\ny {theta}\nrotate\nx {t * .3}\nmove\n250 250 -250\napply\n')
+    f.write(f'save\nframe{str(t).zfill(3) }.png\n')
+
+for t in range(50):
+    f.write(f'clear\ntorus\n0 0 0 50 {50 + 50 * t}\nident\nrotate\nx 30\nrotate\ny 30\nmove\n250 250 -250\napply\n')
+    f.write(f'save\nframe{str(t+100).zfill(3) }.png\n')
+
+f.close()
---
+++ gen.sh
@@ -0,0 +1,5 @@
+make clean
+python gen.py
+make
+cd img
+convert * test.gif
