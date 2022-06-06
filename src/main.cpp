910787b70fb04842966984b8d14e9eab834d9ed5
alvinli04 <alvin.li.j@gmail.com>
2022-06-06 13:23:28 -0400

    refactored

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include <map>
 #include <string>
 #include <sstream>
+#include <array>
+#include <variant>
 
 #include "screen.hpp"
 #include "draw.hpp"
@@ -22,6 +24,8 @@ struct symbol {
     constants c;
     double val;
 };
+
+using command = std::variant<double, std::string>;
 
 int main (int argc, char** argv) {
     picture S (N, N);
@@ -69,23 +73,34 @@ int main (int argc, char** argv) {
    	// parse the command list
    	std::ifstream cmdin ("src/compiler/mdl.cmd");
     std::string cmd, none, knob;
+    std::vector<command> cmdlist;
+    while (cmdin >> cmd) {
+        if (std::isdigit(cmd[0]) || cmd[0] == '-'){
+            cmdlist.push_back(command(stod(cmd)));
+        } else {
+            cmdlist.push_back(command(cmd));
+        }
+    }
+
+    const int num_cmd = cmdlist.size();
+    int cnt = 0;
 
     // pass 0
     bool vary_h = 0, basename_h = 0, frames_h = 0;
-    while (cmdin >> cmd) {
-        if (cmd != "vary" && cmd != "frames" && cmd != "basename") {
-            //skip the line
-            cmdin.ignore(256, '\n');
-        } else if (cmd == "frames") {
-            cmdin >> frames;
+    while (cnt < num_cmd) {
+        cmd = (cmdlist[cnt].index() == 1 ? std::get<std::string>(cmdlist[cnt]) : "");
+        if (cmd == "frames") {
+            ++cnt;
+            frames = std::get<double>(cmdlist[cnt]);
             frames_h = 1;
         } else if (cmd == "basename") {
-            cmdin >> basename;
+            ++cnt;
+            basename = std::get<std::string>(cmdlist[cnt]);
             basename_h = 1;
         } else if (cmd == "vary") {
             vary_h = 1;
-            cmdin.ignore(256, '\n');
         }
+        ++cnt;
     }
 
     if (vary_h && !frames_h) {
@@ -99,28 +114,24 @@ int main (int argc, char** argv) {
     // pass 1
     std::vector<std::map<std::string, double>> frame_list(frames);
 
-    // reset file stream
-    cmdin.clear();
-    cmdin.seekg(0);
+    // reset cnt
+    cnt = 0;
 
-    while (cmdin >> cmd) {
-
-        if (cmd != "vary") {
-            //skip the line
-            cmdin.ignore(512, '\n');
-        } else {
-            std::string knob_name;
-            double sframe, eframe;
-            double sval, eval;
-            cmdin >> sframe >> eframe >> sval >> eval >> knob_name;
+    while (cnt < num_cmd) {
+        cmd = (cmdlist[cnt].index() == 1 ? std::get<std::string>(cmdlist[cnt]) : "");
+        if (cmd == "vary") {
+            std::string knob_name = std::get<std::string>(cmdlist[cnt + 5]);
+            double sframe = std::get<double>(cmdlist[cnt + 1]), eframe = std::get<double>(cmdlist[cnt + 2]);
+            double sval = std::get<double>(cmdlist[cnt + 3]), eval = std::get<double>(cmdlist[cnt + 4]);
             // std::printf("%f, %f, %f, %f ", sframe, eframe, sval, eval);
             // std::cout << knob_name << "]\n";
             for (int i = (int)sframe; i <= (int)eframe; i++) {
                 frame_list[i][knob_name] = sval + (i - (int)sframe) * (eval - sval) / ((int)eframe - (int)sframe);
             }
             symtab[knob_name].val = sval;
+            cnt += 5;
         }
-
+        ++cnt;
     }
     /*
     for(int i = 0; i < frames; i++){
@@ -139,8 +150,7 @@ int main (int argc, char** argv) {
     for (int i = 0; i < frames; i++) {
 
     	// reset file stream
-    	cmdin.clear();
-    	cmdin.seekg(0);
+    	cnt = 0;
 
         // reset zbuffer, cstack
         cstack = std::stack<matrix>();
@@ -150,21 +160,19 @@ int main (int argc, char** argv) {
         S.clear();
         std::fill(zbuffer.begin(), zbuffer.end(), std::vector<double>(N, -std::numeric_limits<double>::infinity()));
 
-	    while (cmdin >> cmd) {
-	        if (cmd == "constants" || cmd == "frames" || cmd == "basename" || cmd == "vary" || cmd == "knob") {
-				//skip the line
-				cmdin.ignore(256, '\n');
-			} else if (cmd == "push") {
-	            cmdin >> none;
+	    while (cnt < num_cmd) {
+            cmd = (cmdlist[cnt].index() == 1 ? std::get<std::string>(cmdlist[cnt]) : "");
+	        if (cmd == "push") {
+	            ++cnt;
 	            matrix m = cstack.top();
 				cstack.push (m);
 	        } else if (cmd == "pop") {
-	            cmdin >> none;
+	            ++cnt;
 	            cstack.pop ();
 	        } else if (cmd == "move") {
-	            double x, y, z;
-				cmdin >> x >> y >> z;
-	            cmdin >> knob;
+	            double x = std::get<double>(cmdlist[cnt + 1]), y = std::get<double>(cmdlist[cnt + 2]), z = std::get<double>(cmdlist[cnt + 3]);
+                knob = std::get<std::string>(cmdlist[cnt + 4]);
+	            cnt += 4;
 	            if (knob != "None") {
 	            	x *= frame_list[i][knob];
 	            	y *= frame_list[i][knob];
@@ -172,24 +180,24 @@ int main (int argc, char** argv) {
 	            }
 				cstack.top() *= move (x, y, z);
 	        } else if (cmd == "rotate") {
-	            char axis;
-	            double theta;
-	            cmdin >> axis >> theta;
-	            cmdin >> knob;
+	            std::string axis = std::get<std::string>(cmdlist[cnt + 1]);
+	            double theta = std::get<double>(cmdlist[cnt + 2]);
+                knob = std::get<std::string>(cmdlist[cnt + 3]);
+	            cnt += 3;
 	            if (knob != "None") {
 	            	theta *= frame_list[i][knob];
 	            }
-	            if (axis == 'x') {
+	            if (axis == "x") {
 	                cstack.top() *= rot_x (theta);
-	            } else if (axis == 'y') {
+	            } else if (axis == "y") {
 	                cstack.top() *= rot_y (theta);
-	            } else if (axis == 'z') {
+	            } else if (axis == "z") {
 	                cstack.top() *= rot_z (theta);
 	            }
 	        } else if (cmd == "scale") {
-	            double x, y, z;
-	            cmdin >> x >> y >> z;
-	            cmdin >> knob;
+                double x = std::get<double>(cmdlist[cnt + 1]), y = std::get<double>(cmdlist[cnt + 2]), z = std::get<double>(cmdlist[cnt + 3]);
+                knob = std::get<std::string>(cmdlist[cnt + 4]);
+                cnt += 4;
 	            if (knob != "None") {
 	            	x *= frame_list[i][knob];
 	            	y *= frame_list[i][knob];
@@ -197,11 +205,15 @@ int main (int argc, char** argv) {
 	            }
 	            cstack.top() *= scale (x, y, z);
 	        } else if (cmd == "box") {
-	            std::string constants;
-	            cmdin >> constants;
-	            cmdin >> none;
-	            double x, y, z, width, height, depth;
-				cmdin >> x >> y >> z >> width >> height >> depth;
+	            std::string constants = std::get<std::string>(cmdlist[cnt + 1]);
+	            ++cnt;
+	            double x = std::get<double>(cmdlist[cnt + 2]),
+                y = std::get<double>(cmdlist[cnt + 3]),
+                z = std::get<double>(cmdlist[cnt + 4]),
+                width = std::get<double>(cmdlist[cnt + 5]),
+                height = std::get<double>(cmdlist[cnt + 6]),
+                depth = std::get<double>(cmdlist[cnt + 7]);
+				cnt += 6;
 				box (x, y, z, width, height, depth, T);
 				T *= cstack.top();
 
@@ -226,11 +238,13 @@ int main (int argc, char** argv) {
 				draw_lines (T, S, WHITE, zbuffer, ambient, lights, ka_r, kd_r, ks_r, ka_g, kd_g, ks_g, ka_b, kd_b, ks_b);
 				T.clear();
 	        } else if (cmd == "sphere") {
-	            std::string constants;
-	            cmdin >> constants;
-	            cmdin >> none;
-	            double x, y, z, r;
-				cmdin >> x >> y >> z >> r;
+	            std::string constants = std::get<std::string>(cmdlist[cnt + 1]);
+	            ++cnt;
+	            double x = std::get<double>(cmdlist[cnt + 2]),
+                y = std::get<double>(cmdlist[cnt + 3]),
+                z = std::get<double>(cmdlist[cnt + 4]),
+                r = std::get<double>(cmdlist[cnt + 5]);
+                cnt += 4;
 				sphere (x, y, z, r, T);
 				T *= cstack.top();
 
@@ -255,11 +269,14 @@ int main (int argc, char** argv) {
 				draw_lines (T, S, WHITE, zbuffer, ambient, lights, ka_r, kd_r, ks_r, ka_g, kd_g, ks_g, ka_b, kd_b, ks_b);
 				T.clear();
 	        } else if (cmd == "torus") {
-	            std::string constants;
-	            cmdin >> constants;
-	            cmdin >> none;
-	            double x, y, z, r1, r2;
-				cmdin >> x >> y >> z >> r1 >> r2;
+                std::string constants = std::get<std::string>(cmdlist[cnt + 1]);
+	            ++cnt;
+	            double x = std::get<double>(cmdlist[cnt + 2]),
+                y = std::get<double>(cmdlist[cnt + 3]),
+                z = std::get<double>(cmdlist[cnt + 4]),
+                r1 = std::get<double>(cmdlist[cnt + 5]),
+                r2 = std::get<double>(cmdlist[cnt + 6]);
+                cnt += 5;
 				torus (x, y, z, r1, r2, T);
 				T *= cstack.top();
 
@@ -284,26 +301,20 @@ int main (int argc, char** argv) {
 				draw_lines (T, S, WHITE, zbuffer, ambient, lights, ka_r, kd_r, ks_r, ka_g, kd_g, ks_g, ka_b, kd_b, ks_b);
 				T.clear();
 	        } else if (cmd == "line") {
-	            cmdin >> none;
-	            double x0, y0, z0, x1, y1, z1;
-				cmdin >> x0 >> y0 >> z0 >> x1 >> y1 >> z1;
+	            ++cnt;
+	            double x0 = std::get<double>(cmdlist[cnt + 1]),
+                y0 = std::get<double>(cmdlist[cnt + 2]),
+                z0 = std::get<double>(cmdlist[cnt + 3]),
+                x1 = std::get<double>(cmdlist[cnt + 4]),
+                y1 = std::get<double>(cmdlist[cnt + 5]),
+                z1 = std::get<double>(cmdlist[cnt + 6]);
+                cnt += 6;
 				E.add_edge (x0, y0, z0, x1, y1, z1);
 				E *= cstack.top();
 				draw_lines (E, S, WHITE, zbuffer);
 				E.clear();
 	        }
-
-	        // save
-	        std::string num = std::to_string(i);
-	        for (int j = 0; j < 3 - std::to_string(i).size(); j++)
-	        	num = "0" + num;
-	        std::string fname = "img/" + basename + num + ".png";
-			S.to_ppm (fname);
-			std::string fsave = "convert " + fname + ".ppm " + fname;
-			std::system (fsave.c_str ());
-			std::string fdel = "rm " + fname + ".ppm";
-			std::system (fdel.c_str ());
-
+            ++cnt;
 	        /*
 	        else if (cmd == "save") {
 	            std::string fname;
@@ -322,6 +333,16 @@ int main (int argc, char** argv) {
 	        */
 	        // std::cout << cmd << '\n';
 	    }
+        // save
+        std::string num = std::to_string(i);
+        for (int j = 0; j < 3 - std::to_string(i).size(); j++)
+            num = "0" + num;
+        std::string fname = "img/" + basename + num + ".png";
+        S.to_ppm (fname);
+        std::string fsave = "convert " + fname + ".ppm " + fname;
+        std::system (fsave.c_str ());
+        std::string fdel = "rm " + fname + ".ppm";
+        std::system (fdel.c_str ());
 
 	    // update knob values in the symbol table
         for (auto& p : frame_list[i]) {
